7b84cb3c5999c3f1a2d657c1b5ec494dae54e326
Harsh Jagdishbhai Kevadia <kevadiyaharsh@gmail.com>
2017-11-30 15:07:54 -0500

    Runnig React

--- cms/src/App.js
+++ cms/src/App.js
@@ -1,19 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
+//import logo from './logo.svg';
+//import './App.css';
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+//import "../node_modules/jquery/dist/";
+//import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import jQuery from 'jquery';
+import "bootstrap";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header>
+            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+              <a className="navbar-brand" href="#">
+                CMS
+              </a>
+            
+              <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Home <span class="sr-only">(current)</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </header>
+
+          <div className="App-body">
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Switch>
+              {/* <Route path="/404" component={Error404} />
+            <Route path="/" component={} /> */}
+            </Switch>
+          </div>
+          <footer>@Copyrights 2018</footer>
+        </div>
+      </Router>
     );
   }
 }
--- cms/src/index.js
+++ cms/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const title = "CMS";
+
+ReactDOM.render(<App title={title} author="Harsh Jagdishbhai Kevadia"/>, document.getElementById('root'));
 registerServiceWorker();
